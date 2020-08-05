ac7ea8bee41f7d6ea0c4111f1c363e5d64c08529
Nakano-Ryoko <68225988+Nakano-Ryoko@users.noreply.github.com>
2020-08-05 19:04:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@ self.addEventListener('install', function(event) {
       .then(function(cache) {
         return cache.addAll([
           '/',
-          '/styles.css',
+          'app.css',
           'app.js',
-          '/image/default.png'
+          'image/icon.png'
         ]);
       })
   );
