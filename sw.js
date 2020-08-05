e18a6313c3e4737088866b656e56c47771d31ce8
Nakano-Ryoko <68225988+Nakano-Ryoko@users.noreply.github.com>
2020-08-05 17:00:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,18 +6,3 @@ console.log('Hello from sw.js');
 // ファイブラリのインポート
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
-// ファイルのキャッシュ
-workbox.precaching.precacheAndRoute([
-  {
-    url: '/index.html',
-    revision: '12345'
-  },
-  {
-    url: '/app.css',
-    revision: '12345'
-  },
-  { 
-　url: '/app.js',
-    revision: '12345'
-  },
-])
