2b5397d8118a1e2f9fb73289d3fc6e0d7f1d5f2c
Nakano-Ryoko <68225988+Nakano-Ryoko@users.noreply.github.com>
2020-08-05 19:22:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(event) {
     caches.open('cache-v1')
       .then(function(cache) {
         return cache.addAll([
-          '/',
+          'index.html',
           'app.css',
           'app.js',
           'image/icon.png'
