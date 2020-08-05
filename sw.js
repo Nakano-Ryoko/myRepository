baa03a57e930338214feafa10a8ad123ccf05ad7
Nakano-Ryoko <68225988+Nakano-Ryoko@users.noreply.github.com>
2020-08-05 17:51:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,12 +6,3 @@ console.log('Hello from sw.js');
 // ファイブラリのインポート
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
-// Service Worker インストール時に実行される
-self.addEventListener('install', (event) => {
-    console.log('service worker install ...');
-});
-
-// Service Worker アクティベート時に実行される
-self.addEventListener('activate', (event) => {
-  console.info('activate', event);
-});
