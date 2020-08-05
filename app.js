a329ab68cfd476059bb79c322a709ee8d36e5887
Nakano-Ryoko <68225988+Nakano-Ryoko@users.noreply.github.com>
2020-08-05 22:23:38 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
    window.addEventListener('load', function() {
-     navigator.serviceWorker.register('/sw.js')
+     navigator.serviceWorker.register('/firebase-messaging-sw.js')
        .then(function(registration) {
          // 登録成功
          console.log('ServiceWorker 登録成功: ', registration.scope);
