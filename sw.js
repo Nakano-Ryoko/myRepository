5e597a2587f5c87cd4bd5ecb5d93ae6404d78b44
Nakano-Ryoko <68225988+Nakano-Ryoko@users.noreply.github.com>
2020-08-13 08:22:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,22 +13,5 @@ self.addEventListener('activate', function(e) {
 });
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {
-	console.log(event.request.url);
-});
-
-
-// Service Workerが有効なブラウザである場合のみ実行
-if ("serviceWorker" in navigator) {
-  // Service Workerの登録
-  // register()の引数にはService Workerの処理が書かれたファイルのpathを指定
-  navigator.serviceWorker
-    .register("sw.js")
-    .then(function (registration) {
-      console.log("Success ! Scope: ", registration.scope);
-    })
-    .catch(function (err) {
-      console.log("Failed ! Error: ", err);
-    });
-}
+self.addEventListener('fetch', function(event) {});
 
