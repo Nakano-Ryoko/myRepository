9a14be07cb7b131c6eca3017b8474b1ebf282bc4
Nakano-Ryoko <68225988+Nakano-Ryoko@users.noreply.github.com>
2020-08-13 10:04:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 ﻿//ホーム画面にアイコンを表示
 
-self.addEventListener('fetch', function(event) {
-});
+//self.addEventListener('fetch', function(event) {});
 
 // service-worker.js
 self.addEventListener('install', function(e) {
@@ -11,6 +10,9 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 //self.addEventListener('fetch', function(event) {});
